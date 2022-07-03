39b2c4828a5e5acb2c34414d867b6e7e3665a882
Mohammad Feryzal Fahlevi <mferyzalfahlevi29@gmail.com>
2022-07-03 19:55:20 +0700

    view modal edit jabatan

---
+++ resources/views/jabatan2/modal-edit-jabatan.blade.php
@@ -0,0 +1,8 @@
+<div class="form-group">
+    <label for="kode" class="col-form-label">Kode Jabatan</label>
+    <input type="text" class="form-control" id="kode" name="kode" value="{{$jabatan->id}}" readonly>
+</div>
+<div class="form-group">
+    <label for="jabatan" class="col-form-label">Jabatan</label>
+    <input type="text" class="form-control" id="jabatan" name="jabatan" value="{{$jabatan->jabatan}}" required>
+</div>
