f1792628c905506f9a6040def8e8f3b20217102e
Mohammad Feryzal Fahlevi <mferyzalfahlevi29@gmail.com>
2022-07-11 11:59:48 +0700

    Menambahkan Favicon

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="{{ asset('image/setting/logo.ico') }}">
   <title>E-Office | Log in</title>
 
   <!-- Google Font: Source Sans Pro -->
